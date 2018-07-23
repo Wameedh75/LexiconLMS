95f0944bc4318439af17c28886a67015f8d542be
Wameedh75 <wameedh75@yahoo.com>
2018-07-23 10:55:23 +0200

    Modoles

---
+++ LexiconLMS/Migrations/201807230812102_AddCourseIdToMoudle.cs
@@ -0,0 +1,20 @@
+namespace LexiconLMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCourseIdToMoudle : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Modules", name: "Course_Id", newName: "CourseId");
+            RenameIndex(table: "dbo.Modules", name: "IX_Course_Id", newName: "IX_CourseId");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.Modules", name: "IX_CourseId", newName: "IX_Course_Id");
+            RenameColumn(table: "dbo.Modules", name: "CourseId", newName: "Course_Id");
+        }
+    }
+}
