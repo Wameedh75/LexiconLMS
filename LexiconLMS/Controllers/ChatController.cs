de18d9f69b8c6ee35a53c56c46dfab135358e834
Houuse <Mohamed.almohsen@outlook.com>
2018-08-07 17:01:13 +0200

    add views

--- LexiconLMS/Controllers/ChatController.cs
+++ LexiconLMS/Controllers/ChatController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LexiconLMS.Models;
+using LexiconLMS.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,39 @@ namespace LexiconLMS.Controllers
         public ActionResult Index()
         {
             return View("Chat");
+        }
+        public ActionResult LandingPage()
+        {
+            var fakechat = new ChatsListViewModel()
+            {
+                Chats = new List<Chat>() { new Chat() { ChatName = "First Chat", ChatId = "xxx" } }
+            };
+            var fakemessages = new MessagesListViewModel()
+            {
+                Messages = new List<Message>()
+                {
+                    new Message() { Content = "lets test",User=new ApplicationUser() { FirstName="john" , LastName= "Smith"} }
+                ,   new Message() { Content = "second messagw test",User=new ApplicationUser() { FirstName="Dimitris" , LastName= "Bjorlingh"} }
+
+                }
+            };
+            var lpobj = new LandingPageViewModel() { ChatsList = fakechat, MessagesList = fakemessages };
+            return View("SendMessage", lpobj);
+        }
+
+        public ActionResult Messages(string chatId)
+        {
+
+            if (chatId!=null)
+            {
+                using (var db = ApplicationDbContext.Create())
+                {
+                    var MessageList = db.Messages.Where(m => m.ChatId == chatId).ToList();
+
+                    return PartialView("_MessagesList", MessageList);
+                }
+            }
+            return PartialView("_MessagesList");
         }
     }
 }
---
+++ LexiconLMS/ViewModels/LandingPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.ViewModels
+{
+    public class LandingPageViewModel
+    {
+        public ChatsListViewModel ChatsList { get; set; }
+        public MessagesListViewModel MessagesList { get; set; }
+    }
+}
