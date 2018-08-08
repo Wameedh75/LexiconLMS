4cd5a893cc939fc3ccfff09fdfaf035184f52a49
Robert Huselius <robert@huseli.us>
2018-08-08 11:08:42 +0200

    Add Activity and Activity Documents working

--- LexiconLMS/Controllers/ModulesController.cs
+++ LexiconLMS/Controllers/ModulesController.cs
@@ -12,32 +12,27 @@ namespace LexiconLMS.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Modules
-        public ActionResult Index(int? courseId)
-        {
-            var couseModels = db.Modules.Where(m => m.CourseId == courseId);
-            return View(couseModels);
+        public ActionResult Index(int? courseId) {
+            var courseModels = db.Modules.Where(m => m.CourseId == courseId);
+            return View(courseModels);
         }
 
         // GET: Modules/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        public ActionResult Details(int? id) {
+            if (id == null) {
+                return RedirectToAction("Index", "Courses");
             }
             Module module = db.Modules.Find(id);
-            if (module == null)
-            {
+            if (module == null) {
                 return HttpNotFound();
             }
             return View(module);
         }
 
         // GET: Modules/Create
-        public ActionResult Create(int id)
-        {
+        public ActionResult Create(int id) {
             var course = db.Courses.Find(id);
-            return View(new Module { CourseId = id, StartDate = DateTime.Today, EndDate = DateTime.Today,Course = course});
+            return View(new Module { CourseId = id, StartDate = DateTime.Today, EndDate = DateTime.Today, Course = course });
         }
 
         // POST: Modules/Create
@@ -45,11 +40,9 @@ namespace LexiconLMS.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Description,StartDate,EndDate,CourseId")] Module module)
-        {
+        public ActionResult Create([Bind(Include = "Id,Name,Description,StartDate,EndDate,CourseId")] Module module) {
 
-            if (ModelState.IsValid)
-            {
+            if (ModelState.IsValid) {
                 db.Modules.Add(module);
                 //db.Courses.Find(courseId)?.CourseModules.Add(module);
                 db.SaveChanges();
@@ -60,15 +53,12 @@ namespace LexiconLMS.Controllers
         }
 
         // GET: Modules/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Edit(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Module module = db.Modules.Find(id);
-            if (module == null)
-            {
+            if (module == null) {
                 return HttpNotFound();
             }
             return View(module);
@@ -79,10 +69,8 @@ namespace LexiconLMS.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Description,StartDate,EndDate,CourseId")] Module module)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,StartDate,EndDate,CourseId")] Module module) {
+            if (ModelState.IsValid) {
                 db.Entry(module).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Details", "Courses", new { id = module.CourseId });
@@ -91,15 +79,12 @@ namespace LexiconLMS.Controllers
         }
 
         // GET: Modules/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Delete(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Module module = db.Modules.Find(id);
-            if (module == null)
-            {
+            if (module == null) {
                 return HttpNotFound();
             }
             return View(module);
@@ -108,8 +93,7 @@ namespace LexiconLMS.Controllers
         // POST: Modules/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
+        public ActionResult DeleteConfirmed(int id) {
             Module module = db.Modules.Find(id);
             var courseId = module.CourseId;
             db.Documents.RemoveRange(db.Documents.Where(d => d.ModuleId == id));
@@ -119,10 +103,8 @@ namespace LexiconLMS.Controllers
             return RedirectToAction("Details", "Courses", new { id = courseId });
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
                 db.Dispose();
             }
             base.Dispose(disposing);
